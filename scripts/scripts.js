78bd3044b6575cd37550caa2b78d3f698650e549
elena-castello <elena.castello1@gmail.com>
2017-12-01 20:54:45 -0500

    Friday night fever
    final edits on friday - buttons, file structure, photos, radio buttons appear out of clicked buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,23 @@
-// jQuery hide()/show() example with a switch statement
+ // jQuery hide()/show() example with a switch statement
 
-//$(document).ready(function() {
-
-
-  // function to hide all divs
-  //function hideAll() {
-    //$('#name1').hide();
-    //$('#name2').hide();
-    //$('#name3').hide();
-    //$('#name4').hide();
-  //}
-
-  // run that function right away
-  //hideAll();
-
-  // when any animal is clicked, make the suitable div appear
-  //$('.classname').click(function() {
-    //hideAll();
-
-    // here is a switch statement - this was in Codecademy
-    // "Control Flow" lesson 7
+$(document).ready(function() {
+  $(".form-group").hide();
 
 
-  //$("button").click(function() {
-    //$('#name5').slideToggle();
-//});
+  $( ".myButton" ).click(function() {
+    // if ( $(this).parent().children(".form-group").is(':visible') ) {
+      $(this).parent().children(".form-group").show();
+    // } else {
+      // $(this).parent().children(".form-group").hide();
+    // }
+
+    // switch ($(this).attr("id")) {
+    //   case "form-group":
+    //     $('#form-group').show();
+    //     break;
+    //   }
+  });
+
+
+
+}); // end document ready
